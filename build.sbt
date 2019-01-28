58b6358e6bb88e3e0da005bb1d73b982389b6707
IndiscriminateCoding <28496046+IndiscriminateCoding@users.noreply.github.com>
2019-01-28 14:47:33 +0300

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,15 @@
+organization := "machines"
+name := "machines"
+scalaVersion := "2.12.8"
+scalacOptions += "-language:higherKinds"
+version := "0.0.4"
+
+addCompilerPlugin("org.spire-math" %% "kind-projector" % "0.9.8")
+addCompilerPlugin("com.olegpy" %% "better-monadic-for" % "0.2.4")
+
+libraryDependencies ++= Seq(
+  "org.typelevel" %% "cats-core" % "1.5.0",
+
+  "org.scalatest" %% "scalatest" % "3.0.5" % Test,
+  "org.scalacheck" %% "scalacheck" % "1.14.0" % Test
+)
---
+++ src/test/scala/machines/machines/PlanTSpec.scala
@@ -0,0 +1,27 @@
+package machines.machines
+
+import cats._
+import machines.machines.Is._
+import machines.machines.PlanT.Await
+import org.scalatest.FlatSpec
+
+class PlanTSpec extends FlatSpec {
+  it should "be stack-safe" in {
+    def inf: PlanT[Id, Is[Int, ?], String, Unit] =
+      PlanT.await
+        .flatMap { x: Int => PlanT.emit[Id, Is[Int, ?], String](x.toString) }
+        .flatMap { _ => inf }
+
+    def done[A] = (_: A) => ()
+
+    val emit: (String, Id[Unit]) => Id[Unit] = {
+      case (s, _) => println(s)
+    }
+    val await = new Await[Id, Is[Int, ?], Unit] {
+      def apply[Z](e: Z => Id[Unit], k: Is[Int, Z], f: Id[Unit]): Id[Unit] = e(k lr 1)
+    }
+    val stop: Id[Unit] = ()
+
+    inf(done, emit, await, stop)
+  }
+}

5cc4072dcfb104b253ea3aec5998fe4511556ab6
IndiscriminateCoding <28496046+IndiscriminateCoding@users.noreply.github.com>
2019-04-12 17:20:39 +0300

    Category instance for descriptor.Is

--- src/main/scala/machines/descriptor/Is.scala
+++ src/main/scala/machines/descriptor/Is.scala
@@ -1,5 +1,7 @@
 package machines.descriptor
 
+import cats.arrow.Category
+
 sealed trait Is[-A, +B] {
   def apply(x: A): B
 }
@@ -10,4 +12,12 @@ object Is {
   }
 
   def refl[A]: Is[A, A] = reflAny.asInstanceOf[Is[A, A]]
+
+  implicit def isCategoryInstance: Category[Is] = new Category[Is] {
+    def id[A]: Is[A, A] = refl
+
+    def compose[A, B, C](f: Is[B, C], g: Is[A, B]): Is[A, C] = new Is[A, C] {
+      def apply(x: A): C = f(g(x))
+    }
+  }
 }
